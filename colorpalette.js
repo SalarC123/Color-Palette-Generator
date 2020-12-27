a7ed35ef311539821810add407c9fcdb1c56f13f
SalarC123 <Tabinda@chaudrys-iMac.local>
2020-12-26 21:18:24 -0600

    JS GSAP Animations + Option to delete saved palettes

--- colorpalette.js
+++ colorpalette.js
@@ -1,3 +1,20 @@
+//GSAP Animation
+const t1 = new TimelineMax()
+
+t1.fromTo('#button-tabs', 1, {x: '-300'}, {x:'0', ease: 'sine.out'})
+.fromTo('#button-text', 1, {x: '300'}, {x:'0', ease: 'sine.out'}, '-=1')
+.fromTo('#color-list', 1, {y:'-600'}, {y:'0', ease: 'sine.out'}, '-=1')
+.fromTo('#button-tabs', 1, {padding:'100px'}, {padding:'5px', ease:'sine.out'}, '-=1')
+
+
+
+// Common DOM Elements
+const libraryul = document.querySelector('#saved-palettes')
+const savePopup = document.querySelector('#save-popup')
+const libraryPopup = document.querySelector('#library-popup')
+
+
+
 function generateColors() {
     for (let i = 1; i < 5 + 1; i++) {
         let newColor = `rgb(${Math.floor(Math.random()*255) + 1}, ${Math.floor(Math.random()*255) + 1}, ${Math.floor(Math.random()*255) + 1})`
@@ -9,36 +26,35 @@ function generateColors() {
 
 function savePalette() {
     closeLibraryPopup()
-    savePopup = document.querySelector('#save-popup')
     savePopup.style.display = 'block'
     blurBackground()
 }
 
 function closeSavePopup() {
-    document.querySelector('#save-popup').style.display = 'none'
+    savePopup.style.display = 'none'
     unblurBackground()
 }
 
 function library() {
     closeSavePopup()
-    libraryPopup = document.querySelector('#library-popup')
     libraryPopup.style.display = 'block'
     blurBackground()
 }
 
 function closeLibraryPopup() {
-    document.querySelector('#library-popup').style.display = 'none'
+    libraryPopup.style.display = 'none'
     unblurBackground()
 }
+
+
 
 let liCounter = 1
 function transferToLibrary() {
     // Adds button to library
-    libraryul = document.querySelector('#saved-palettes')
-    inputText = document.querySelector('#name-of-palette').value
+    let inputText = document.querySelector('#name-of-palette').value
     if (inputText) {
         document.querySelector('#name-of-palette').value = ''
-        libraryul.innerHTML += `<li><button onclick='colorsToMain(${liCounter})'>${inputText}</button></li>`
+        libraryul.innerHTML += `<li><button>${inputText}</button></li>`
     }
 
     
@@ -55,9 +71,13 @@ function transferToLibrary() {
         colorBlock.style.height = '35px'
         colorBlock.style.backgroundColor = color.innerHTML
     }
-    libraryulli.innerHTML += '<h2>X</h2>'
+
+    // Adds red X for deleting saved palettes
+    libraryulli.innerHTML += "<h2 class='delete-from-library'>X</h2>"
     liCounter++
 }
+
+
 
 const blurredItems = ['#button-text', '#button-tabs', '#color-list', '#footer']
 
@@ -75,20 +95,34 @@ function unblurBackground() {
     }
 }
 
+
+
 function randomClassName() {
     return Math.random().toString(36).substr(2,10)
 }
 
-function colorsToMain(childNum) {
-    // const li = document.querySelector(`#saved-palettes li:nth-child(${childNum})`)
-    const colors = document.querySelectorAll(`#saved-palettes li:nth-child(${childNum}) span`)
-    const colorHolders = document.querySelectorAll('#color-list li')
-    colors.forEach((span, index) => {
-        let color = span.style.backgroundColor
-        let colorHolderLi = Array.from(colorHolders)[index]
-        colorHolderLi.innerHTML = color
-        colorHolderLi.style.backgroundColor = color
-    })
-}
 
-// EVENT LISTENERS
+
+// Listens for button clicks to transfer saved palette to main screen
+libraryul.addEventListener('click', function (e) {
+    if (e.target instanceof HTMLButtonElement) {
+        let elem = e.target.parentElement
+        let childNum = Array.from(elem.parentElement.children).indexOf(elem) + 1
+        const colors = document.querySelectorAll(`#saved-palettes li:nth-child(${childNum}) span`)
+        const colorHolders = document.querySelectorAll('#color-list li')
+        colors.forEach((span, index) => {
+            let color = span.style.backgroundColor
+            let colorHolderLi = Array.from(colorHolders)[index]
+            colorHolderLi.innerHTML = color
+            colorHolderLi.style.backgroundColor = color
+        })
+    }
+})
+
+// Listens for clicks on delete palette button in library
+libraryul.addEventListener('click', e => {
+    if (e.target instanceof HTMLHeadingElement) {
+        libraryul.removeChild(e.target.parentElement)
+        liCounter--
+    }
+})
